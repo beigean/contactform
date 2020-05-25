6e261b1da7f09fe6405bcdf9300b279987625fd7
naopin <naopin708@yahoo.co.jp>
2020-05-25 22:54:09 +0900

    vue.js

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <div id="app">
+     <Information></Information>
+   </div>
+</template>
+
+<script>
+
+import Information from './conponents/Information.vue';
+
+export default {
+  
+   name: "",
+   components: {
+      Information
+   },
+    methods: {
+
+    }
+};
+
+</script>
+
+<style>
+
+</style>
