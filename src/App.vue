291592a39392af50bb4dc9c82061763757807cd1
naopin <naopin708@yahoo.co.jp>
2020-05-29 11:18:19 +0900

    機能追加

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <template>
-   <div id="app">
-     <Information></Information>
-   </div>
+  <div id="app">
+    <router-view> </router-view>
+  </div>
 </template>
 
-<script>
 
-import Information from './conponents/Information.vue';
-
-export default {
-  
-   name: "",
-   components: {
-      Information
-   },
-    methods: {
-
-    }
-};
-
-</script>
-
-<style>
-
-</style>
