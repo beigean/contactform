09f48fff524f66ec4d6bb65c03b350bfb4c26f45
goro9 <sokenbiccha@icloud.com>
2020-06-01 22:18:10 +0900

    save vuex test

--- src/components/Sex.vue
+++ src/components/Sex.vue
@@ -2,9 +2,9 @@
   <div id="sex">
     <p>お客様の情報を入力してください</p>
     <p>-性別-</p>
-    <input type="radio" id="men" v-model="sex" value="男性" v-on:change="onChange({sex: sex})"/>
+    <input type="radio" id="men" v-model="sex" value="男性" v-on:change="updateAnswer"/>
     <label for="men">男性</label>
-    <input type="radio" id="woman" v-model="sex" value="女性" v-on:change="onChange({sex: sex})"/>
+    <input type="radio" id="woman" v-model="sex" value="女性" v-on:change="updateAnswer"/>
     <label for="woman">女性</label>
   </div>
 </template>
@@ -18,5 +18,11 @@ export default {
     };
   },
   props: ['onChange'],
+  methods: {
+    updateAnswer(event) {
+      console.log(event);
+      this.$store.dispatch('updateAnswers', {sex: event.target.value});
+    }
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    answers: {
+      sex: "",
+      birthday: {
+         year: 0,
+         month: 0,
+         day: 0,
+      },
+      insurance: "",
+      hospitalized_now: "",
+      hospitalized_past: "",
+      consultation: "",
+    },
   },
   mutations: {
+    updateAnswers(state, updateObject) {
+      state.answers = Object.assign({}, state.answers, updateObject);
+    },
   },
   actions: {
+    updateAnswers({ commit }, updateObject) {
+      commit('updateAnswers', updateObject);
+    }
   },
   modules: {
   }
